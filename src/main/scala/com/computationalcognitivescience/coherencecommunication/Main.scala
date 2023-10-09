01d7640bc95dd24b884789557fb59c0b0155376e
Christian Bloks <50616562+ChrisBloks@users.noreply.github.com>
2023-10-09 11:03:05 +0200

    Added exclamation marks

--- src/main/scala/com/computationalcognitivescience/coherencecommunication/Main.scala
+++ src/main/scala/com/computationalcognitivescience/coherencecommunication/Main.scala
@@ -2,6 +2,6 @@ package com.computationalcognitivescience.coherencecommunication
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello from main of class")
+    println("Hello from main of class!!")
   }
 }
