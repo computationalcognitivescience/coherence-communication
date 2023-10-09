f3234a8e7f4acefcc10b9de0594e6908f3fa2b1e
Mark Blokpoel <mark.blokpoel@gmail.com>
2023-10-09 11:20:08 +0200

    random graph example

---
+++ src/main/scala/com/computationalcognitivescience/coherencecommunication/GraphExample.scala
@@ -0,0 +1,17 @@
+package com.computationalcognitivescience.coherencecommunication
+
+import mathlib.graph._
+import mathlib.graph.GraphImplicits._
+object GraphExample {
+
+  def main(args: Array[String]): Unit = {
+    val network = WUnDiGraph.random(10, 0.5)
+    println("Vertices")
+    network.vertices.foreach(println)
+    println("Edges")
+    network.edges.foreach(println)
+
+    
+  }
+
+}
