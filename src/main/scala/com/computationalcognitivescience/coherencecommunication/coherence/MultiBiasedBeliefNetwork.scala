9140d987b8a202833534ea04ea6d284cf6eb8057
Mark Blokpoel <mark.blokpoel@gmail.com>
2023-12-06 15:06:30 +0100

    Draft implementation and demo

---
+++ src/main/scala/com/computationalcognitivescience/coherencecommunication/coherence/MultiBiasedBeliefNetwork.scala
@@ -0,0 +1,46 @@
+package com.computationalcognitivescience.coherencecommunication.coherence
+
+import mathlib.graph.{Node, WUnDiEdge, WUnDiGraph}
+import mathlib.set.SetTheory.{ImplAny, sum}
+
+case class BeliefBias(
+    valueAssignment: Map[Node[String], Boolean],
+    weights: Map[Node[String], Double]
+) {
+  val beliefs: Set[Node[String]] = valueAssignment.keys.toSet
+}
+
+class MultiBiasedBeliefNetwork(
+    network: WUnDiGraph[String],
+    negativeConstraints: Set[WUnDiEdge[Node[String]]],
+    val multiBeliefBiases: Seq[BeliefBias]
+) extends BeliefNetwork(network, negativeConstraints) {
+
+  override def coh(
+      assignment: Map[Node[String], Boolean]
+  ): Double =
+    cohPlus(assignment) + cohMin(assignment) +
+      multiBeliefBiases
+        .map(beliefBias => cohBias(assignment, beliefBias))
+        .sum
+
+  private def cohBias(
+      assignment: Map[Node[String], Boolean],
+      beliefBias: BeliefBias
+  ): Double = {
+    def consistentWithBias(belief: Node[String]): Double = {
+      if (
+        assignment.isDefinedAt(belief) &&
+        beliefBias.beliefs.contains(belief) &&
+        assignment(belief) == beliefBias.beliefs(belief)
+      )
+        beliefBias.weights.getOrElse(belief, 0.0)
+      else 0.0
+    }
+
+    sum(beliefBias.beliefs, consistentWithBias _)
+  }
+
+
+
+}
